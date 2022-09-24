1071bd98e18279234ab38519087193f150c409fe
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-09-23 23:43:37 -0300

    practica parcial

---
+++ Clase03/App.js
@@ -0,0 +1,141 @@
+//Array original
+let arrayJson= JSON.parse('[{"id":1, "nombre":"Marcelo", "apellido":"Luque", "edad":45, "titulo":"Ingeniero", "facultad":"UTN", "añoGraduacion":2002},{"id":2, "nombre":"Ramiro", "apellido":"Escobar", "edad":35, "titulo":"Medico", "facultad":"UBA", "añoGraduacion":20012},{"id":3, "nombre":"Facundo", "apellido":"Cairo", "edad":30, "titulo":"Abogado", "facultad":"UCA", "añoGraduacion":2017},{"id":4, "nombre":"Fernando", "apellido":"Nieto", "edad":18, "equipo":"Independiente", "posicion":"Delantero", "cantidadGoles":7},{"id":5, "nombre":"Manuel", "apellido":"Loza", "edad":20, "equipo":"Racing", "posicion":"Volante", "cantidadGoles":2},{"id":6, "nombre":"Nicolas", "apellido":"Serrano", "edad":23, "equipo":"Boca", "posicion":"Arquero", "cantidadGoles":0}]');
+let arrayJugadores=[];
+let arrayProfesionales=[];
+
+//Selectores
+let body = document.querySelector("body");
+
+
+//Asignacion de listeners
+window.addEventListener("load",CargaInformacionJSON);
+window.addEventListener("load",CargarTablas);
+
+function CargaInformacionJSON()
+{
+    arrayJson.forEach(element => {
+        if(element.hasOwnProperty("equipo"))
+        {
+            jugadorNuevo = new Futbolista(element["id"],element["nombre"],element["apellido"],element["edad"],element["equipo"],element["posicion"],element["cantidadGoles"])
+            arrayJugadores.push(jugadorNuevo);
+        }else{
+            profesionalNuevo = new Profesional(element["id"],element["nombre"],element["apellido"],element["edad"],element["titulo"],element["facultad"],element["añoGraduacion"]);
+            arrayProfesionales.push(profesionalNuevo);
+        }
+    });
+}
+
+function CargarTablas()
+{
+    arrayJugadores.forEach(element => {
+        let filaTabla = document.createElement("tr");
+        let celdaId = document.createElement("td");
+        let celdaNombre= document.createElement("td");
+        let celdaApellido = document.createElement("td");
+        let celdaEdad = document.createElement("td");
+        let celdadEquipo = document.createElement("td");
+        let celdaPosicion =document.createElement("td");
+        let celdaGoles = document.createElement("td");
+        let celdaTitulo = document.createElement("td");
+        let celdaFacultad = document.createElement("td");
+        let celdaGraduacion = document.createElement("td");
+        filaTabla.appendChild(celdaId);
+        filaTabla.appendChild(celdaNombre);
+        filaTabla.appendChild(celdaApellido);
+        filaTabla.appendChild(celdaEdad);
+        filaTabla.appendChild(celdadEquipo);
+        filaTabla.appendChild(celdaPosicion);
+        filaTabla.appendChild(celdaGoles);
+        filaTabla.appendChild(celdaTitulo);
+        filaTabla.appendChild(celdaFacultad);
+        filaTabla.appendChild(celdaGraduacion);
+
+        celdaId.innerText=element.id;
+        celdaId.classList.add("numeros");
+        celdaNombre.innerText=element.nombre;
+        celdaNombre.classList.add("texto_largo");
+        celdaApellido.innerText=element.apellido;
+        celdaApellido.classList.add("texto_largo");
+        celdaEdad.innerText=element.edad;
+        celdaEdad.classList.add("numeros");
+        celdadEquipo.innerText= element.equipo;
+        celdadEquipo.classList.add("texto_corto");
+        celdaPosicion.innerText=element.posicion;
+        celdaPosicion.classList.add("texto_corto");
+        celdaGoles.innerText=element.cantidadGoles;
+        celdaGoles.classList.add("numeros");
+        celdaTitulo.innerText="------";
+        celdaTitulo.classList.add("texto_largo");
+        celdaFacultad.innerText="----";
+        celdaFacultad.classList.add("texto_largo");
+        celdaGraduacion.innerText="----";
+        celdaGraduacion.classList.add("texto_corto");
+        
+        document.getElementById("tabla").appendChild(filaTabla);
+     });
+}
+
+
+
+//Clases
+
+class Persona{
+    id;
+    nombre;
+    apellido;
+    edad;
+    
+    constructor (id,nombre,apellido,edad)
+    {
+        this.id=id;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.edad=edad;
+    }
+
+    toString() {
+            return `ID: ${this.id} - Nombre: ${this.nombre} - Apellido: ${this.apellido} - Edad ${this.edad}\n `;
+    }
+   
+}
+
+
+class Futbolista extends Persona{
+    
+    equipo;
+    posicion;
+    cantidadGoles;
+
+    constructor(id,nombre,apellido,edad,equipo,posicion,cantidadGoles)
+    {
+        super(id,nombre,apellido,edad);
+        this.equipo=equipo;
+        this.posicion=posicion;
+        this.cantidadGoles=cantidadGoles;
+    }
+
+    toString()
+    {
+        return `${super.toString()}Equipo: ${this.equipo} - Posicion ${this.posicion} - Goles ${this.cantidadGoles}`;
+    }
+}
+
+
+class Profesional extends Persona{
+
+    titulo;
+    facultad;
+    anoGraduacion;
+    constructor(id,nombre,apellido,edad,titulo,facultad,anoGraduacion)
+    {
+        super(id,nombre,apellido,edad);
+        this.titulo=titulo;
+        this.facultad = facultad;
+        this.anoGraduacion =anoGraduacion;
+    }
+
+    toString()
+    {
+        return `${super.toString()}Titulo: ${this.titulo} - Facultad ${this.facultad} - Año de graduación ${this.anoGraduacion}`;
+    }
+}
