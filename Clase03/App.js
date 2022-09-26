b833a555a8ecd012b5121f07c89d718f9a86ad1e
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-09-26 19:34:20 -0300

    filtro columas listo

--- Clase03/App.js
+++ Clase03/App.js
@@ -1,15 +1,20 @@
 //Array original
 let arrayJson= JSON.parse('[{"id":1, "nombre":"Marcelo", "apellido":"Luque", "edad":45, "titulo":"Ingeniero", "facultad":"UTN", "anoGraduacion":2002},{"id":2, "nombre":"Ramiro", "apellido":"Escobar", "edad":35, "titulo":"Medico", "facultad":"UBA", "anoGraduacion":2012},{"id":3, "nombre":"Facundo", "apellido":"Cairo", "edad":30, "titulo":"Abogado", "facultad":"UCA", "anoGraduacion":2017},{"id":4, "nombre":"Fernando", "apellido":"Nieto", "edad":18, "equipo":"Independiente", "posicion":"Delantero", "cantidadGoles":7},{"id":5, "nombre":"Manuel", "apellido":"Loza", "edad":20, "equipo":"Racing", "posicion":"Volante", "cantidadGoles":2},{"id":6, "nombre":"Nicolas", "apellido":"Serrano", "edad":23, "equipo":"Boca", "posicion":"Arquero", "cantidadGoles":0}]');
-let arrayJugadores=[];
-let arrayProfesionales=[];
+let arrayPersonas=[];
 
 //Selectores
 let body = document.querySelector("body");
 let checkBoxList = document.querySelectorAll('input[type=checkbox]');
+let comboBox = document.getElementById("select_filtro");
+let tablaInformacion = document.getElementById("tabla");
 
 //Asignacion de listeners
-//window.addEventListener("load",CargaInformacionJSON);
+window.addEventListener("load",CargaInformacionJSON);
 window.addEventListener("load",CargarTablas);
+comboBox.addEventListener("change",CargarTablas)
+checkBoxList.forEach(element => {
+    element.addEventListener("change",FiltrarColumnas);
+});
 
 function CargaInformacionJSON()
 {
@@ -17,86 +22,172 @@ function CargaInformacionJSON()
         if(element.hasOwnProperty("equipo"))
         {
             jugadorNuevo = new Futbolista(element["id"],element["nombre"],element["apellido"],element["edad"],element["equipo"],element["posicion"],element["cantidadGoles"])
-            arrayJugadores.push(jugadorNuevo);
+            arrayPersonas.push(jugadorNuevo);
         }else{
-            profesionalNuevo = new Profesional(element["id"],element["nombre"],element["apellido"],element["edad"],element["titulo"],element["facultad"],element["añoGraduacion"]);
-            arrayProfesionales.push(profesionalNuevo);
+            profesionalNuevo = new Profesional(element["id"],element["nombre"],element["apellido"],element["edad"],element["titulo"],element["facultad"],element["anoGraduacion"]);
+            arrayPersonas.push(profesionalNuevo);
         }
     });
+    FiltrarColumnas();
 }
-
-function ValidarJugador(element)
-{
-    if(element.hasOwnProperty("equipo"))
-    {
-        return true;
-    }
-    return false;
-}
-
 function FiltrarColumnas()
 {
-    checkBoxList.forEach(element => {
-        if(element.checked)
-        {
-            console.log(element.value);
-        }
+    document.querySelectorAll(".id").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".nombre").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".apellido").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".edad").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".equipo").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".posicion").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".goles").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".titulo").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".facultad").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".ano_graduacion").forEach(a=>a.style.display = "none");
+    let checkBoxChecked = document.querySelectorAll('input[type=checkbox]:checked');
+    checkBoxChecked.forEach(element => {
+        if(element.value == "id")
+            document.querySelectorAll(".id").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "nombre")
+            document.querySelectorAll(".nombre").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "apellido")
+            document.querySelectorAll(".apellido").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "edad")
+            document.querySelectorAll(".edad").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "equipo")
+            document.querySelectorAll(".equipo").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "posicion")
+            document.querySelectorAll(".posicion").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "cantGoles")
+            document.querySelectorAll(".goles").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "titulo")
+            document.querySelectorAll(".titulo").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "facultad")
+            document.querySelectorAll(".facultad").forEach(a=>a.style.display = "inline-block");
+        if(element.value == "anoGraduado")
+            document.querySelectorAll(".ano_graduacion").forEach(a=>a.style.display = "inline-block");
+
     });
 }
+
+
+function FiltrarPorComboBox(element){
+    switch(comboBox.value){
+        case "todos":
+            return true;
+        case "futbolistas":
+            return(element instanceof(Futbolista))
+        case "profesionales":
+            return(element instanceof(Profesional))
+    }
+}
+
 
 function CargarTablas()
 {
-    arrayJson.forEach(element => {
-        esJugador= ValidarJugador(element);
-        let filaTabla = document.createElement("tr");
-        let celdaId = document.createElement("td");
-        let celdaNombre= document.createElement("td");
-        let celdaApellido = document.createElement("td");
-        let celdaEdad = document.createElement("td");
-        let celdadEquipo = document.createElement("td");
-        let celdaPosicion =document.createElement("td");
-        let celdaGoles = document.createElement("td");
-        let celdaTitulo = document.createElement("td");
-        let celdaFacultad = document.createElement("td");
-        let celdaGraduacion = document.createElement("td");
-        filaTabla.appendChild(celdaId);
-        filaTabla.appendChild(celdaNombre);
-        filaTabla.appendChild(celdaApellido);
-        filaTabla.appendChild(celdaEdad);
-        filaTabla.appendChild(celdadEquipo);
-        filaTabla.appendChild(celdaPosicion);
-        filaTabla.appendChild(celdaGoles);
-        filaTabla.appendChild(celdaTitulo);
-        filaTabla.appendChild(celdaFacultad);
-        filaTabla.appendChild(celdaGraduacion);
-
-        celdaId.innerText=element.id;
-        celdaId.classList.add("numeros");
-        celdaNombre.innerText=element.nombre;
-        celdaNombre.classList.add("texto_largo");
-        celdaApellido.innerText=element.apellido;
-        celdaApellido.classList.add("texto_largo");
-        celdaEdad.innerText=element.edad;
-        celdaEdad.classList.add("numeros");
-
-        celdadEquipo.innerText= esJugador ? element.equipo : "-------";
-        celdadEquipo.classList.add("texto_corto");
-        celdaPosicion.innerText=esJugador ? element.posicion : "-------";
-        celdaPosicion.classList.add("texto_corto");
-        celdaGoles.innerText=esJugador ? element.cantidadGoles : "-------"
-        celdaGoles.classList.add("numeros");
-
-        celdaTitulo.innerText= !esJugador ? element.titulo : "-------"
-        celdaTitulo.classList.add("texto_largo");
-        celdaFacultad.innerText=!esJugador ? element.facultad : "-------"
-        celdaFacultad.classList.add("texto_largo");
-        celdaGraduacion.innerText=!esJugador ? element.anoGraduacion : "-------"
-        celdaGraduacion.classList.add("texto_corto");
-        
-        document.getElementById("tabla").appendChild(filaTabla);
-     });
+    tablaInformacion.innerHTML=""; 
+    FiltrarColumnas()
+    CargarTitulos();
+    arrayFiltrado = arrayPersonas.filter(element => FiltrarPorComboBox(element));
+    arrayFiltrado.map(element=>CrearRegistros(element));  
 }
 
+function CrearRegistros(element)
+{
+    let filaTabla = document.createElement("tr");
+    let celdaId = document.createElement("td");
+    let celdaNombre= document.createElement("td");
+    let celdaApellido = document.createElement("td");
+    let celdaEdad = document.createElement("td");
+    let celdadEquipo = document.createElement("td");
+    let celdaPosicion =document.createElement("td");
+    let celdaGoles = document.createElement("td");
+    let celdaTitulo = document.createElement("td");
+    let celdaFacultad = document.createElement("td");
+    let celdaGraduacion = document.createElement("td");
+    filaTabla.appendChild(celdaId);
+    filaTabla.appendChild(celdaNombre);
+    filaTabla.appendChild(celdaApellido);
+    filaTabla.appendChild(celdaEdad);
+    filaTabla.appendChild(celdadEquipo);
+    filaTabla.appendChild(celdaPosicion);
+    filaTabla.appendChild(celdaGoles);
+    filaTabla.appendChild(celdaTitulo);
+    filaTabla.appendChild(celdaFacultad);
+    filaTabla.appendChild(celdaGraduacion);
+
+    celdaId.innerText=element.id;
+    celdaNombre.innerText=element.nombre;   
+    celdaApellido.innerText=element.apellido;   
+    celdaEdad.innerText=element.edad;
+    celdadEquipo.innerText= element instanceof Futbolista ? element.equipo : "-------";   
+    celdaPosicion.innerText=element instanceof Futbolista? element.posicion : "-------";   
+    celdaGoles.innerText=element instanceof Futbolista ? element.cantidadGoles : "-------"   
+    celdaTitulo.innerText= element instanceof Profesional ? element.titulo : "-------"   
+    celdaFacultad.innerText=element instanceof Profesional ? element.facultad : "-------"   
+    celdaGraduacion.innerText=element instanceof Profesional ? element.anoGraduacion : "-------"
+
+    celdaId.classList.add("id");
+    celdaNombre.classList.add("nombre");
+    celdaApellido.classList.add("apellido");
+    celdaEdad.classList.add("edad");
+    celdadEquipo.classList.add("equipo");
+    celdaPosicion.classList.add("posicion");
+    celdaGoles.classList.add("goles");
+    celdaTitulo.classList.add("titulo");
+    celdaFacultad.classList.add("facultad");
+    celdaGraduacion.classList.add("ano_graduacion");
+    
+    tablaInformacion.appendChild(filaTabla);
+}
+
+function CargarTitulos()
+{
+    let filaTitulos = document.createElement("tr");
+    let celdaId = document.createElement("th");
+    let celdaNombre= document.createElement("th");
+    let celdaApellido = document.createElement("th");
+    let celdaEdad = document.createElement("th");
+    let celdadEquipo = document.createElement("th");
+    let celdaPosicion =document.createElement("th");
+    let celdaGoles = document.createElement("th");
+    let celdaTitulo = document.createElement("th");
+    let celdaFacultad = document.createElement("th");
+    let celdaGraduacion = document.createElement("th");
+
+    filaTitulos.appendChild(celdaId);
+    filaTitulos.appendChild(celdaNombre);
+    filaTitulos.appendChild(celdaApellido);
+    filaTitulos.appendChild(celdaEdad);
+    filaTitulos.appendChild(celdadEquipo);
+    filaTitulos.appendChild(celdaPosicion);
+    filaTitulos.appendChild(celdaGoles);
+    filaTitulos.appendChild(celdaTitulo);
+    filaTitulos.appendChild(celdaFacultad);
+    filaTitulos.appendChild(celdaGraduacion);
+
+    celdaId.classList.add("id");
+    celdaNombre.classList.add("nombre");
+    celdaApellido.classList.add("apellido");
+    celdaEdad.classList.add("edad");
+    celdadEquipo.classList.add("equipo");
+    celdaPosicion.classList.add("posicion");
+    celdaGoles.classList.add("goles");
+    celdaTitulo.classList.add("titulo");
+    celdaFacultad.classList.add("facultad");
+    celdaGraduacion.classList.add("ano_graduacion");
+
+    celdaId.innerText="ID";
+    celdaNombre.innerText="Nombre";
+    celdaApellido.innerText="Apellido";
+    celdaEdad.innerText="Edad";
+    celdadEquipo.innerText="Equipo";
+    celdaPosicion.innerText="Posicion";
+    celdaGoles.innerText="Goles";
+    celdaTitulo.innerText="Titulo";
+    celdaFacultad.innerText="Facultad";
+    celdaGraduacion.innerText="Año Graduación";
+
+    tablaInformacion.appendChild(filaTitulos);
+}
 
 
 //Clases
